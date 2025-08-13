eb0608a20e031b06b4ce838fe43a656ae18bf910
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-14 00:33:28 +0600

    12th commit

--- total.js
+++ total.js
@@ -15,3 +15,22 @@ function getprice(shop){
 
 const liam = getprice(shop);
 console.log(liam);
+
+
+
+const plus = [
+    {name:"pant",Price:700,quantity:2},
+    {name:"shirt",Price:300,quantity:3},
+    {name:"punjabi",Price:1200,quantity:1},
+
+];
+
+function getprice1(plus){
+    let total=0;
+    for(const i of plus){
+        const totalcost = i.Price*i.quantity;
+        total = total + totalcost;
+    }return total;
+}
+const siam = getprice1(plus);
+console.log(siam);
