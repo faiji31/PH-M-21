9a8d01c79eba17bd2b70d80ae6db91da6c08f186
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-13 21:46:25 +0600

    3rd commit

--- liam.js
+++ liam.js
@@ -6,3 +6,21 @@ if (liam>juba){
 else{
     console.log("juba Got Hieghest Marks");
 }
+
+
+// function use for max number
+function maxi(num1,num2){
+    if(num1>num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+const liam1 = maxi(71,87);
+console.log(liam1);
+const siam = maxi(32,21);
+console.log(siam);
+
+
+const max = Math.max(12,43,32,56,78);
+console.log(max);
