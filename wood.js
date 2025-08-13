cbf59ec1fb7167a0bb08919a1a5bbcdbb660fa18
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-13 23:11:14 +0600

    7th commit

--- wood.js
+++ wood.js
@@ -1,13 +1,29 @@
-function quntity(chair,bed,table){
-    const perchair=3;
-    const pertable=10;
-    const perbed= 40;
-    const chairtotalwood = perchair * chair;
-    const tabletotalwood = pertable * table;
-    const bedtotalwood = perbed * bed;
+// function quntity(chair,bed,table){
+//     const perchair=3;
+//     const pertable=10;
+//     const perbed= 40;
+//     const chairtotalwood = perchair * chair;
+//     const tabletotalwood = pertable * table;
+//     const bedtotalwood = perbed * bed;
 
-    const totalwood = chairtotalwood + tabletotalwood + bedtotalwood;
-    return totalwood;
+//     const totalwood = chairtotalwood + tabletotalwood + bedtotalwood;
+//     return totalwood;
+// }
+// const price = quntity(2,5,3);
+// console.log(price);
+// shirt pant price claculate
+
+function shirtquantity(shirt,pant,shorts){
+    const shirtq =300;
+    const pantq = 600;
+    const shortsq = 400;
+    const shirtprice = shirt * shirtq;
+    const pantprice = pant * pantq;
+    const shortsprice = shorts * shortsq;
+
+    const total = shirtprice + pantprice + shortsprice;
+    return total;
+
 }
-const price = quntity(2,5,3);
-console.log(price);
+const liam = shirtquantity(2,2,2);
+console.log(liam);
